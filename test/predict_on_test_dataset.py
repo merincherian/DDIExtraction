25f05016e48faa55886caa3167a1ece0dbbb33c6
Merin Cherian <38310878+merincherian@users.noreply.github.com>
2024-02-28 13:21:52 +0000

    Update predict_on_test_dataset.py

--- test/predict_on_test_dataset.py
+++ test/predict_on_test_dataset.py
@@ -5,7 +5,10 @@ from training.train import extract_training_data_from_dataframe, trained_model_p
 import pandas as pd
 import os
 def predict():
-    df = get_dataset_dataframe(directory=os.path.expanduser('~/ddi/dataset/DDICorpus/Test/test_for_ddi_extraction_task/DrugBank/'))
+    new_path = os.path.join(os.getcwd(), "dataset/DDICorpus/Test/test_for_ddi_extraction_task/DrugBank/")
+    # df = get_dataset_dataframe(directory=os.path.expanduser('~/ddi/dataset/DDICorpus/Test/test_for_ddi_extraction_task/DrugBank/'))
+    df = get_dataset_dataframe(directory=new_path)
+    
     X, Y = extract_training_data_from_dataframe(df)
     model = pd.read_pickle(trained_model_pickle_file)
     y_pred  = model.predict(X)
